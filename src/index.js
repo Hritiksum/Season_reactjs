e8eddad4b5eebcad8998b43e0ef506326a24f23f
Hritiksum <hritik.sum@gmail.com>
2020-10-20 16:48:55 +0530

    Class,constructor added and added location fetch

--- src/index.js
+++ src/index.js
@@ -3,15 +3,27 @@ import ReactDOM from 'react-dom';
 
 import SeasonDisplay from './SeasonDisplay';
 
-const App=()=>{
-    return(
-        <div>
-            Hi there!
-            <br/>
+class App extends React.Component{
+    constructor(props){
+        super(props);
+        this.state={lat: null};
+        this.state={lon: null};
+
+        window.navigator.geolocation.getCurrentPosition(
+            position=>{
+                this.setState({lat: position.coords.latitude,lon: position.coords.longitude});
+                
+            },
+            err=>console.log(err)
+        );
+    }
+    render(){
+        
+        return <div>
+            Latitude and longitude: {this.state.lat}, {this.state.lon}
             <SeasonDisplay/>
         </div>
-        
-    );
-};
+    }
+}
 
 ReactDOM.render(<App/>,document.querySelector('#root'));
